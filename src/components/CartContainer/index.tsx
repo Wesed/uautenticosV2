eaf8d0df93bd791ad528ad1306d6ff5b5b06be84
weslley <dev.wfeduardo@gmail.com>
2023-12-20 12:01:36 -0300

    animando abertura e fechamento do carrinho

--- src/components/CartContainer/index.tsx
+++ src/components/CartContainer/index.tsx
@@ -7,6 +7,7 @@ import { useCart } from '@/contexts/cart-context'
 import { ProductContainer } from './product-container'
 import Link from 'next/link'
 import { priceFormatter } from '@/utils/priceFormatter'
+import { AnimatePresence, motion } from 'framer-motion'
 
 export function CartContainer() {
   const { items } = useCart()
@@ -53,101 +54,108 @@ export function CartContainer() {
         </span>
       </button>
 
-      <div
-        className={twMerge(
-          'fixed right-0 top-0 z-10 flex-col justify-center',
-          'min-h-screen w-[480px]',
-          'rounded-md bg-gray800 px-12 shadow-3xl',
-          `${open ? 'flex' : 'hidden'}`,
-        )}
-      >
-        <button
-          onClick={() => {
-            setOpen(!open)
-          }}
-          className='absolute right-6 top-6 text-gray400 transition-colors hover:text-green500'
-        >
-          <X className='h-6 w-6' />
-        </button>
+      <AnimatePresence>
+        {open && (
+          <motion.div
+            initial={{ scale: 0 }}
+            animate={{ scale: open ? 1 : 0 }}
+            exit={{ scale: 0 }}
+            className={twMerge(
+              'fixed right-0 top-0 z-10 flex-col justify-center',
+              'min-h-screen w-[480px]',
+              'rounded-md bg-gray800 px-12 shadow-3xl',
+              `${open ? 'flex' : 'hidden'}`,
+            )}
+          >
+            <button
+              onClick={() => {
+                setOpen(!open)
+              }}
+              className='absolute right-6 top-6 text-gray400 transition-colors hover:text-green500'
+            >
+              <X className='h-6 w-6' />
+            </button>
 
-        {items.length > 0 ? (
-          <>
-            <h2 className='mb-8 mt-[72px] text-xl font-bold text-gray100'>
-              Sacola de compras
-            </h2>
+            {items.length > 0 ? (
+              <>
+                <h2 className='mb-8 mt-[72px] text-xl font-bold text-gray100'>
+                  Sacola de compras
+                </h2>
 
-            <div className='flex flex-col gap-6'>
-              {items.map((prod) => (
-                <ProductContainer
-                  key={prod.productId}
-                  id={prod.productId}
-                  image={prod.imageUrl}
-                  description={prod.name}
-                  size={prod.size}
-                  price={prod.price}
-                  quantity={prod.quantity}
-                />
-              ))}
-            </div>
+                <div className='flex flex-col gap-6'>
+                  {items.map((prod) => (
+                    <ProductContainer
+                      key={prod.productId}
+                      id={prod.productId}
+                      image={prod.imageUrl}
+                      description={prod.name}
+                      size={prod.size}
+                      price={prod.price}
+                      quantity={prod.quantity}
+                    />
+                  ))}
+                </div>
 
-            <div className='mt-8 flex flex-col gap-2'>
-              Possui cupom de desconto?
-              <input
-                type='text'
-                className={twMerge(
-                  'w-full rounded-md',
-                  'bg-gray900 p-2',
-                  'border-0 outline-none',
-                )}
-              />
-            </div>
+                <div className='mt-8 flex flex-col gap-2'>
+                  Possui cupom de desconto?
+                  <input
+                    type='text'
+                    className={twMerge(
+                      'w-full rounded-md',
+                      'bg-gray900 p-2',
+                      'border-0 outline-none',
+                    )}
+                  />
+                </div>
 
-            <footer className='mb-12 mt-auto flex w-full flex-col gap-2'>
-              <div className='flex justify-between'>
-                <span className='text-gray100'>Quantidade</span>
-                <span className=''>{items.length} itens</span>
-              </div>
+                <footer className='mb-12 mt-auto flex w-full flex-col gap-2'>
+                  <div className='flex justify-between'>
+                    <span className='text-gray100'>Quantidade</span>
+                    <span className=''>{items.length} itens</span>
+                  </div>
 
-              <div className='flex justify-between'>
-                <span className='text-lg font-bold'>Valor total</span>
-                <span className='text-2xl font-bold'>
-                  {priceFormatter(getTotalCartValue)}
+                  <div className='flex justify-between'>
+                    <span className='text-lg font-bold'>Valor total</span>
+                    <span className='text-2xl font-bold'>
+                      {priceFormatter(getTotalCartValue)}
+                    </span>
+                  </div>
+
+                  <button
+                    // onClick={handleBuyProduct}
+                    // disabled={isCreatingCheckout}
+                    className={twMerge(
+                      'mt-5 cursor-pointer rounded-lg p-5',
+                      'font-bold uppercase text-white',
+                      'transition-colors',
+                      'bg-green500 enabled:hover:bg-green300',
+                      'disabled:cursor-not-allowed disabled:opacity-70',
+                    )}
+                  >
+                    Finalizar Pedido
+                  </button>
+                </footer>
+              </>
+            ) : (
+              <div className='flex flex-col gap-4 self-center text-gray300'>
+                O seu carrinho está vazio!
+                <span className=''>
+                  <Link
+                    href='/'
+                    className='font-bold transition-colors hover:text-white'
+                    onClick={() => {
+                      setOpen(false)
+                    }}
+                  >
+                    Clique aqui
+                  </Link>{' '}
+                  para comprar!
                 </span>
               </div>
-
-              <button
-                // onClick={handleBuyProduct}
-                // disabled={isCreatingCheckout}
-                className={twMerge(
-                  'mt-5 cursor-pointer rounded-lg p-5',
-                  'font-bold uppercase text-white',
-                  'transition-colors',
-                  'bg-green500 enabled:hover:bg-green300',
-                  'disabled:cursor-not-allowed disabled:opacity-70',
-                )}
-              >
-                Finalizar Pedido
-              </button>
-            </footer>
-          </>
-        ) : (
-          <div className='flex flex-col gap-4 self-center text-gray300'>
-            O seu carrinho está vazio!
-            <span className=''>
-              <Link
-                href='/'
-                className='font-bold transition-colors hover:text-white'
-                onClick={() => {
-                  setOpen(false)
-                }}
-              >
-                Clique aqui
-              </Link>{' '}
-              para comprar!
-            </span>
-          </div>
+            )}
+          </motion.div>
         )}
-      </div>
+      </AnimatePresence>
     </div>
   )
 }
---
+++ variants.ts
@@ -0,0 +1,9 @@
+export const fadeIn = (direction: string, delay: number) => {
+  return {
+    const variants = {
+  open: { opacity: 1, x: 0 },
+  closed: { opacity: 0, x: "-100%" },
+}
+
+  }
+}
